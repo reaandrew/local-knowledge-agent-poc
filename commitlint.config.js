c2ea5187a6aa29e2a1c92c5e575e99b31170f30f
Andy Rea <email@andrewrea.co.uk>
2025-04-22 15:03:13 +0100

    fix: Increase commit message body line length limit

--- commitlint.config.js
+++ commitlint.config.js
@@ -24,6 +24,7 @@ module.exports = {
     'subject-case': [2, 'always', ['sentence-case']],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
-    'header-max-length': [2, 'always', 72]
+    'header-max-length': [2, 'always', 72],
+    'body-max-line-length': [2, 'always', 200]
   }
 }; 
